a47391bff4da730fcf3cde9fad9daccb98e592ee
Krish <abc@gmail.com>
2025-10-05 18:27:52 +0530

    completed

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:10000/api',
+  baseURL: ' https://personal-finance-manager-backend-l8v8.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
