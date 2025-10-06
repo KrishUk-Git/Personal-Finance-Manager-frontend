6b5e14ee27325f688a960bcf35661273b3712775
Krish <abc@gmail.com>
2025-10-06 16:00:09 +0530

    completed

--- src/services/api.js
+++ src/services/api.js
@@ -1,20 +1,24 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://personal-finance-manager-backend-4uij.onrender.com/api',
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers['x-auth-token'] = token;
+// Add a request interceptor to include the token in all API requests
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['x-auth-token'] = token;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+);
 
 export default api;
